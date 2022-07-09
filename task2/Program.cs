5ba0210613180bae9fe5ea4e7c7cfaf2276a4731
«Natali» <feichka@yandex.ru>
2022-07-09 23:40:45 +0300

    Add Task2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+a = Convert.ToInt32(Console.ReadLine());
+int b;
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+
