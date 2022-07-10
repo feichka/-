f6a90b08b3d1e333e79406846564044ede6da4c7
«Natali» <feichka@yandex.ru>
2022-07-10 12:57:39 +0300

    Add task 4

---
+++ task4/Program.cs
@@ -0,0 +1,23 @@
+﻿int a;
+a = Convert.ToInt32(Console.ReadLine());
+int b;
+b = Convert.ToInt32(Console.ReadLine());
+int c;
+c = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+if (a > b)
+{
+    i = a;
+}
+else
+{
+    i = b;
+}
+if (c > i)
+{
+    Console.WriteLine(c);
+}
+else
+{
+    Console.WriteLine(i);
+}
