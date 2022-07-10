237f21e7dff893e6d5e5d40d845cf48c34686c95
«Natali» <feichka@yandex.ru>
2022-07-10 13:26:07 +0300

    Add task8

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= n)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+        i = i + 2;
+    }
+    else
+    {
+        i = i + 1;
+    }
+}
